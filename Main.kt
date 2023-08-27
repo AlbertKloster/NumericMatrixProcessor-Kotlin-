7876044ace6dd32cdbdfa6675327aef650c5ff33
AlbertKloster <albert.kloster@gmail.com>
2023-08-27 13:31:21 -0400

    stage2

--- Main.kt
+++ Main.kt
@@ -1,17 +1,22 @@
 package processor
 
 fun main() {
-    val (rows1, columns1) = readln().split(" ").map { it.toInt() }
+    val (rows1, _) = readln().split(" ").map { it.toInt() }
     val matrix1 = List(rows1) { readln().split(" ").map { it.toInt() } }
 
-    val (rows2, columns2) = readln().split(" ").map { it.toInt() }
-    val matrix2 = List(rows2) { readln().split(" ").map { it.toInt() } }
+    val number = readln().toInt()
 
-    if (rows1 != rows2 || columns1 != columns2) {
-        println("ERROR")
-    } else {
-        val sum = matrix1.zip(matrix2) { row1, row2 -> row1.zip(row2) { e1, e2 -> e1 + e2 } }
-        sum.forEach { println(it.joinToString(" ")) }
-    }
+    matrix1.multiply(number).forEach { println(it.joinToString(" ")) }
 
 }
+
+
+private fun List<List<Int>>.multiply(other: List<List<Int>>): List<List<Int>> {
+    if (this.size != other.size || this.first().size != other.first().size)
+        throw RuntimeException("ERROR")
+    else
+        return this.zip(other) { row1, row2 -> row1.zip(row2) { e1, e2 -> e1 + e2 } }
+}
+
+private fun List<List<Int>>.multiply(number: Int) = this.map { row -> row.map { it * number } }
+
